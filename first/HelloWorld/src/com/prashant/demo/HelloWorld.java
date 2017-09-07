c00b2087232435de16195270f85c4a3c11eecd7e
Prashant Savadi <prashant@testmile.com>
2017-09-07 15:13:32 +0530

    This is testing commit

---
+++ first/HelloWorld/src/com/prashant/demo/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.prashant.demo;
+
+public class HelloWorld {
+
+	
+public static void main(String[] args) {
+	
+	for(int i= 0; i< 10; i++)
+	{	
+		System.out.println("This is hello world!!");
+	}	
+	
+}	
+	
+	
+}
